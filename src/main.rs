295c2e1cc66f08df09d65f622861486e4ed945cb
Ameer Hamza <ameernormie@gmail.com>
2020-03-27 23:40:40 +0500

    feat: read the arguments from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mut query = "";
+    let mut filename = "";
+
+    if args.len() == 2 {
+        query = &args[1];
+    }
+    if args.len() > 2 {
+        query = &args[1];
+        filename = &args[2];
+    }
+
+    println!("query is {}", query);
+    println!("filename is {}", filename);
 }
