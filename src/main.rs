2c215d748352480ea64490dbf8470941183b0750
Ameer Hamza <ameernormie@gmail.com>
2020-03-28 15:46:21 +0500

    feat: make a new method on Config struct to make the code more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     let contents =
         fs::read_to_string("poem.txt").expect("Something went wrong while reading the file");
@@ -20,9 +20,11 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
+    }
 }
