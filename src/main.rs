dc21dd51676624013d66efa2ceb7d51b70888e40
Ameer Hamza <ameernormie@gmail.com>
2020-03-28 14:44:54 +0500

    refactor: use a struct to parse_config to make code more understandable

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let mut query = "";
-    let mut filename = "";
-
-    if args.len() == 2 {
-        query = &args[1];
-    }
-    if args.len() > 2 {
-        query = &args[1];
-        filename = &args[2];
-    }
+    let config = parse_config(&args);
 
     let contents =
         fs::read_to_string("poem.txt").expect("Something went wrong while reading the file");
 
     println!("contents of poem {}", contents);
 
-    println!("query is {}", query);
-    println!("filename is {}", filename);
+    println!("query is {}", config.query);
+    println!("filename is {}", config.filename);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
