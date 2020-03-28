23cf0ca93d8197ed784f312eead6e6f66683f694
Ameer Hamza <ameernormie@gmail.com>
2020-03-28 16:29:29 +0500

    feat: add error handling in our config using Result
    use a closure to get the err value.
    use  in the Result.
    use std::process to exit with a code

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    // err is in the closure. err argument appears in vertical pipes if result is Err
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
+    #[allow(unused_variables)]
     let contents =
         fs::read_to_string("poem.txt").expect("Something went wrong while reading the file");
-
-    println!("contents of poem {}", contents);
 
     println!("query is {}", config.query);
     println!("filename is {}", config.filename);
 }
 
+#[derive(Debug)]
+#[allow(dead_code)]
 struct Config {
     query: String,
     filename: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
 
-        Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
-        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
     }
 }
