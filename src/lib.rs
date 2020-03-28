856ed0a678c806d6aa20dd39085202834e34dab8
Ameer Hamza <ameernormie@gmail.com>
2020-03-28 19:32:14 +0500

    feat: add search function to search contents with tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use std::error::Error;
 use std::fs;
 
-#[derive(Debug)]
 #[allow(dead_code)]
+#[derive(Debug)]
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -23,10 +23,36 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
+    // () is called unit type in rust
     // using () like this is the idiomatic way to indicate that we’re calling run
     // for its side effects only; it doesn’t return a value we need.
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+            println!("line : {}", line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
