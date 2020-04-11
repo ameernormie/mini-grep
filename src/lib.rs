1f4cdf6c0a8ebf441e193304cdd208fb47541e88
Ameer Hamza <ameernormie@gmail.com>
2020-04-11 16:43:20 +0500

    refactor: improve program logic by implementing iterators

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        // first argument in the env args is the name of program. So we need to ignore it
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config {
